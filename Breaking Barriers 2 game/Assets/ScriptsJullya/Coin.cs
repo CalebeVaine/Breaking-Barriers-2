76111809f9e4219c6af2c5dc22bac3e164f15e5c
Jullya2 <jullya.monteiro@alunos.sc.senac.br>
2025-10-13 15:12:31 -0300

    .

--- Breaking Barriers 2 game/Assets/ScriptsJullya/Coin.cs
+++ Breaking Barriers 2 game/Assets/ScriptsJullya/Coin.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int points = 1; // Quantos pontos esta moeda vale
+    public int points = 1;
 
     public void Collect()
     {
-        // 1. Adiciona a pontuação no Player
         Player.score += points;
-
-        // 2. Exibe o novo score no Console
         Debug.Log("Ponto coletado! Novo Score: " + Player.score);
-
-        // 3. Destrói o objeto (faz a moeda desaparecer)
         Destroy(gameObject);
     }
 }
--- Breaking Barriers 2 game/Assets/ScriptsJullya/Player.cs
+++ Breaking Barriers 2 game/Assets/ScriptsJullya/Player.cs
@@ -9,80 +9,36 @@ public class Player : MonoBehaviour
 
     public static int score = 0;
 
-    // Referências para o Quiz
-    [SerializeField]
-    private QuizManager quizManager;
-    public int coinsToStartQuiz = 5;
-    private bool quizActive = false;
-
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         score = 0;
-        if (quizManager == null)
-        {
-            quizManager = FindObjectOfType<QuizManager>();
-        }
-        Time.timeScale = 1;
     }
 
     void Update()
     {
-        // Movimento só é permitido se o quiz NÃO estiver ativo
-        if (!quizActive)
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
+            Jump();
         }
     }
 
     void FixedUpdate()
     {
-        float currentSpeed = quizActive ? 0 : horizontal * speed;
-        // Usa 'velocity' (corrigindo o erro de API obsoleta)
+        float currentSpeed = horizontal * speed;
         body.linearVelocity = new Vector2(currentSpeed, body.linearVelocity.y);
     }
 
     void Jump()
     {
-        if (!quizActive)
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
-        }
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
     }
 
-    // Método chamado pelo script da Coin
     public void AddCoin(int points)
     {
         score += points;
         Debug.Log("Ponto coletado! Novo Score: " + score);
-
-        if (score >= coinsToStartQuiz && !quizActive)
-        {
-            StartQuiz();
-        }
-    }
-
-    void StartQuiz()
-    {
-        if (quizManager != null)
-        {
-            quizActive = true;
-            Time.timeScale = 0; // PAUSA o jogo
-            quizManager.ShowQuiz();
-        }
-    }
-
-    // Método que o QuizManager chama para despausar o jogo (corrigindo o erro CS1061 anterior)
-    public void EndQuiz()
-    {
-        quizActive = false;
-        Time.timeScale = 1; // DESPAUSA o jogo
-        score = 0; // Reseta o score para começar a próxima contagem
-        Debug.Log("Quiz finalizado. Jogo resumido.");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -92,8 +48,7 @@ public class Player : MonoBehaviour
             Coin coin = other.GetComponent<Coin>();
             if (coin != null)
             {
-                // CHAMADA CORRIGIDA: Passa a referência do Player ('this')
-                coin.Collect(this);
+                coin.Collect();
             }
         }
     }
